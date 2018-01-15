2b477d1cb43f8f057816a85c2869999d9b486540
Peter Seremetis <pswriter5348@gmail.com>
2018-01-15 18:07:32 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,26 @@
+function shout(string)
+{
+  return string.toUpperCase()
+}
+function whisper(string)
+{
+  return string.toLowerCase('HELLO')
+}
+function logShout(string)
+{
+  console.log('HELLO')
+}
+function logWhisper(string)
+{
+  console.log('hello')
+}
+function sayHiToGrandma(string)
+{
+  var yesIndeed = "YES INDEED!"
+  var iCant = "I can't hear you!"
+  var iLoveYou = "I love you, too."
+  if (string===string.toUpperCase())
+  return yesIndeed;
+  else (string===string.toLowerCase())
+  return iCant;
+}
